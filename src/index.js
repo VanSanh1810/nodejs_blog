c63306552dd9b91672a5f8b890ce964cd784e077
sanh nguyen <sanhnguyen734@gmail.com>
2023-06-01 20:37:25 +0700

    Controllers and Router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+const route = require('./routes')
 
+//Static files
 app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
@@ -15,9 +17,12 @@ app.engine('hbs', handlebars.engine({extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.set('views',path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+//Read form data
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+//Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const newsRouter = require('./news')
+
+function route(app) {
+
+
+    app.use('/', newsRouter)
+    
+    app.get("/", (req, res) => {
+        res.render("home");
+    });
+
+    app.post("/", (req, res) => {
+        console.log(req.body);
+        res.send("");
+    });
+}
+
+module.exports = route;
